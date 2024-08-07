74de11b56f2625ba9643555d2425cd14d64e4749
James Kwon <jhk1097@gmail.com>
2024-08-07 17:56:58 +1200

    Added Sidebar component and initial project setup

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import './Sidebar.css'
+import { assets } from '../../assets/assets'
 
 const Sidebar = () => {
-  return <div className="sidebar">Sidebar</div>
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <img className="menu" src={assets.menu_icon} alt="" />
+      </div>
+      <div className="bottom"></div>
+    </div>
+  )
 }
 
 export default Sidebar
