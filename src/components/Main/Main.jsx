6fb38b2eebe265cef8c321c11130592e9fcc671d
James Kwon <jhk1097@gmail.com>
2024-08-09 16:49:17 +1200

    Added search box and footer message to Main screen

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -36,6 +36,19 @@ const Main = () => {
             <img src={assets.code_icon} alt="" />
           </div>
         </div>
+        <div className="main-bottom">
+          <div className="search-box">
+            <input type="text" placeholder="Enter a prompt here" />
+            <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              <img src={assets.send_icon} alt="" />
+            </div>
+          </div>
+          <p className="bottom-info">
+            JuddyAI delivers, but verify the details that matter most.
+          </p>
+        </div>
       </div>
     </div>
   )
