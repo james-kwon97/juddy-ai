2b36219a8368bd9cd46ad7dbfbc298c910b05a22
James Kwon <jhk1097@gmail.com>
2024-08-10 17:46:10 +1200

    Added state management in ContextProvider for handling input and prompts

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,14 +1,17 @@
-import { createContext } from 'react'
+import { createContext, useState } from 'react'
 import runChat from '../config/juddy'
 
 export const Context = createContext()
 
 const ContextProvider = (props) => {
+  const [input, setInput] = useState('')
+  const [recentPrompt, setRecentPrompt] = useState('')
+  const [previousPrompts, setPreviousPrompts] = useState([])
+  const [showResult, setShowResult] = useState(false)
+
   const onSent = async (prompt) => {
     await runChat(prompt)
   }
-
-  onSent('What is react js')
 
   const contextValue = {}
   return (
