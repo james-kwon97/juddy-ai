9d7934cc1d1e1112af8e2c7f0abd3d68ccd2ddc3
James Kwon <jhk1097@gmail.com>
2024-08-15 12:52:13 +1200

    Adding back safety settings

--- src/config/juddy.js
+++ src/config/juddy.js
@@ -1,57 +1,3 @@
-// import {
-//   GoogleGenerativeAI,
-//   HarmCategory,
-//   HarmBlockThreshold,
-// } from '@google/generative-ai'
-
-// const MODEL_NAME = 'gemini-1.5-flash'
-// const API_KEY = 'AIzaSyCmvJFOodepc1ptLR9nxrDyrORjovn7xvQ'
-
-// async function runChat(prompt) {
-//   const genAI = new GoogleGenerativeAI(API_KEY)
-//   const model = genAI.getGenerativeModel({ model: MODEL_NAME })
-
-//   const generationConfig = {
-//     temperature: 1,
-//     topK: 64,
-//     topP: 0.95,
-//     maxOutputTokens: 8192,
-//     responseMimeType: 'text/plain',
-//   }
-
-//   const safetySettings = [
-//     {
-//       category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-//       threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-//     },
-//     {
-//       category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-//       threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-//     },
-//     {
-//       category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-//       threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-//     },
-//     {
-//       category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-//       threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-//     },
-//   ]
-
-//   const chat = model.startChat({
-//     generationConfig,
-//     safetySettings,
-//     history: [],
-//   })
-
-//   const result = await chat.sendMessage(prompt)
-//   const response = result.response
-//   console.log(response.text())
-//   return response.text()
-// }
-
-// export default runChat
-
 import {
   GoogleGenerativeAI,
   HarmCategory,
@@ -74,11 +20,29 @@ const generationConfig = {
   responseMimeType: 'text/plain',
 }
 
+const safetySettings = [
+  {
+    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+  },
+]
+
 async function runChat(prompt) {
   const chatSession = model.startChat({
     generationConfig,
-    // safetySettings: Adjust safety settings
-    // See https://ai.google.dev/gemini-api/docs/safety-settings
+    safetySettings,
     history: [],
   })
 
@@ -86,8 +50,5 @@ async function runChat(prompt) {
   console.log(result.response.text())
   return result.response.text()
 }
-
-// Example usage:
-// runChat("INSERT_INPUT_HERE");
 
 export default runChat
