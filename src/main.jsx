eedd6eb6d68a52863445ed1425e56f66b36896ac
James Kwon <jhk1097@gmail.com>
2024-08-16 16:22:44 +1200

    Importing the Clerk publishable key into main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import App from './App.jsx'
 import './index.css'
 import ContextProvider from './context/Context.jsx'
 
+// Importing Clerk publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ContextProvider>
     <App />
