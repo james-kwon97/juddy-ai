c9c23332edf00c1da5b996c62135bbb53dc0f0ff
James Kwon <jhk1097@gmail.com>
2024-08-16 16:31:46 +1200

    Integrate Clerk authentication with SignedIn, SignedOut, SignInButton, and UserButton in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import Sidebar from './components/Sidebar/Sidebar'
 import Main from './components/Main/Main'
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from '@clerk/clerk-react'
 
 const App = () => {
   return (
     <>
+      <header>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
       <Sidebar />
       <Main />
     </>
