f4d9ca08fada6b613a19b7341630d3a2a52b7f45
James Kwon <jhk1097@gmail.com>
2024-08-16 16:26:34 +1200

    Adding ClerkProvider to my app

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import ContextProvider from './context/Context.jsx'
+import { ClerkProvider } from '@clerk/clerk-react'
 
 // Importing Clerk publishable key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -10,8 +12,18 @@ if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Publishable Key')
 }
 
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <ContextProvider>
+//     <App />
+//   </ContextProvider>
+// )
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ContextProvider>
-    <App />
-  </ContextProvider>
+  <React.StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <ContextProvider>
+        <App />
+      </ContextProvider>
+    </ClerkProvider>
+  </React.StrictMode>
 )
