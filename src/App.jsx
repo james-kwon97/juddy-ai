f36f379f3bdd3b30bb24a1e7107b9cdff0b82688
James Kwon <jhk1097@gmail.com>
2024-08-18 19:52:29 +1200

    Removed unneccesary imports in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Sidebar from './components/Sidebar/Sidebar'
 import Main from './components/Main/Main'
 import LandingPage from './components/LandingPage/LandingPage'
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  UserButton,
-} from '@clerk/clerk-react'
+import { SignedIn } from '@clerk/clerk-react'
 
 const App = () => {
   return (
