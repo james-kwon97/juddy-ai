280fa6908527675bda8b75c6c18fcfd4ae4ace8f
James Kwon <jhk1097@gmail.com>
2024-08-25 22:21:48 +1200

    Containers have been added to landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -17,29 +17,37 @@ const LandingPage = () => {
 
   return (
     <div className="landing-page">
-      <div className="left-section">
-        <p className="logo">Juddy AI</p>
-        <img
-          src={assets.main_gif}
-          alt="Moving bubbles gif with a black background"
-          className="landing-image"
-        />
-      </div>
-      <div className="right-section right-half">
-        <div className="content">
-          <p className="greet">
-            <span>Welcome to Juddy AI</span>
-          </p>
-          <p className="greet-subtext">Please log in to continue</p>
-          <div className="auth-buttons">
-            <SignedOut>
-              <SignInButton mode="modal">
-                <button className="log-in">Log In</button>
-              </SignInButton>
-              <SignUpButton mode="modal">
-                <button className="sign-up">Sign Up</button>
-              </SignUpButton>
-            </SignedOut>
+      <div className="landing-container">
+        <div className="left-section">
+          <div className="logo-container">
+            <p className="logo">Juddy AI</p>
+          </div>
+          <div className="image-container">
+            <img
+              src={assets.main_gif}
+              alt="Moving bubbles gif with a black background"
+              className="landing-image"
+            />
+          </div>
+        </div>
+        <div className="right-section">
+          <div className="content-container">
+            <div className="content">
+              <p className="greet">
+                <span>Welcome to Juddy AI</span>
+              </p>
+              <p className="greet-subtext">Please log in to continue</p>
+              <div className="auth-buttons">
+                <SignedOut>
+                  <SignInButton mode="modal">
+                    <button className="log-in">Log In</button>
+                  </SignInButton>
+                  <SignUpButton mode="modal">
+                    <button className="sign-up">Sign Up</button>
+                  </SignUpButton>
+                </SignedOut>
+              </div>
+            </div>
           </div>
         </div>
       </div>
